b19320ad7008900e2552ff29a910a334c3652400
Anthony Ramine <n.oxyde@gmail.com>
2017-02-01 10:58:48 +0100

    Format using rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,52 @@
-/*!
-
-This crate provides wrappers and convenience functions to make Hyper and
-Serde work hand in hand.
-
-The supported types are:
-
- * `cookie::Cookie`
- * `hyper::header::ContentType`
- * `hyper::header::Headers`
- * `hyper::http::RawStatus`
- * `hyper::method::Method`
-
-# How do I use a data type with a `Headers` member with Serde?
-
-Use the serde attributes `deserialize_with` and `serialize_with`.
-
-```
-struct MyStruct {
-    #[serde(deserialize_with = "hyper_serde::deserialize",
-            serialize_with = "hyper_serde::serialize")]
-    headers: Headers,
-}
-```
-
-# How do I encode a `Headers` value with `serde_json::to_string`?
-
-Use the `Ser` wrapper.
-
-```
-serde_json::to_string(&Ser::new(&headers))
-```
-
-# How do I decode a `Method` value with `serde_json::parse`?
-
-Use the `De` wrapper.
-
-```
-serde_json::parse::<De<Method>>("\"PUT\"").map(De::into_inner)
-```
-
-# How do I send `Cookie` values as part of an IPC channel?
-
-Use the `Serde` wrapper. It implements `Deref` and `DerefMut` for convenience.
-
-```
-ipc::channel::<Serde<Cookie>>()
-```
-
-*/
+//! This crate provides wrappers and convenience functions to make Hyper and
+//! Serde work hand in hand.
+//!
+//! The supported types are:
+//!
+//! `cookie::Cookie`
+//! `hyper::header::ContentType`
+//! `hyper::header::Headers`
+//! `hyper::http::RawStatus`
+//! `hyper::method::Method`
+//!
+//! # How do I use a data type with a `Headers` member with Serde?
+//!
+//! Use the serde attributes `deserialize_with` and `serialize_with`.
+//!
+//! ```
+//! struct MyStruct {
+//! #[serde(deserialize_with = "hyper_serde::deserialize",
+//! serialize_with = "hyper_serde::serialize")]
+//! headers: Headers,
+//! }
+//! ```
+//!
+//! # How do I encode a `Headers` value with `serde_json::to_string`?
+//!
+//! Use the `Ser` wrapper.
+//!
+//! ```
+//! serde_json::to_string(&Ser::new(&headers))
+//! ```
+//!
+//! # How do I decode a `Method` value with `serde_json::parse`?
+//!
+//! Use the `De` wrapper.
+//!
+//! ```
+//! serde_json::parse::<De<Method>>("\"PUT\"").map(De::into_inner)
+//! ```
+//!
+//! # How do I send `Cookie` values as part of an IPC channel?
+//!
+//! Use the `Serde` wrapper. It implements `Deref` and `DerefMut` for
+//! convenience.
+//!
+//! ```
+//! ipc::channel::<Serde<Cookie>>()
+//! ```
+//!
+//!
 
 #![deny(missing_docs)]
 #![deny(unsafe_code)]
@@ -69,15 +68,18 @@ use std::cmp::PartialEq;
 use std::fmt;
 use std::ops::{Deref, DerefMut};
 
-#[cfg(test)] extern crate serde_test;
-#[cfg(test)] use serde_test::{Token, assert_de_tokens, assert_ser_tokens};
+#[cfg(test)]
+extern crate serde_test;
+#[cfg(test)]
+use serde_test::{Token, assert_de_tokens, assert_ser_tokens};
 
 /// Deserialises a `T` value with a given deserializer.
 ///
 /// This is useful to deserialize Hyper types used in structure fields or
 /// tuple members with `#[serde(deserialize_with = "hyper_serde::deserialize")]`.
 pub fn deserialize<T, D>(deserializer: &mut D) -> Result<T, D::Error>
-    where D: Deserializer, De<T>: Deserialize
+    where D: Deserializer,
+          De<T>: Deserialize,
 {
     De::deserialize(deserializer).map(De::into_inner)
 }
@@ -92,7 +94,9 @@ pub fn deserialize<T, D>(deserializer: &mut D) -> Result<T, D::Error>
 #[cfg_attr(test, derive(PartialEq))]
 pub struct De<T>(T);
 
-impl<T> De<T> where De<T>: Deserialize {
+impl<T> De<T>
+    where De<T>: Deserialize,
+{
     /// Consumes this wrapper, returning the deserialized value.
     #[inline(always)]
     pub fn into_inner(self) -> T {
@@ -102,7 +106,7 @@ impl<T> De<T> where De<T>: Deserialize {
 
 impl Deserialize for De<ContentType> {
     fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
-        where D: Deserializer
+        where D: Deserializer,
     {
         Mime::deserialize(deserializer).map(ContentType).map(De)
     }
@@ -110,7 +114,7 @@ impl Deserialize for De<ContentType> {
 
 impl Deserialize for De<Cookie> {
     fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
-        where D: Deserializer
+        where D: Deserializer,
     {
         struct CookieVisitor;
 
@@ -118,11 +122,11 @@ impl Deserialize for De<Cookie> {
             type Value = De<Cookie>;
 
             fn visit_str<E>(&mut self, v: &str) -> Result<Self::Value, E>
-                where E: Error
+                where E: Error,
             {
-                Cookie::parse(v).map(De).map_err(|e| {
-                    E::custom(format!("{:?}", e))
-                })
+                Cookie::parse(v)
+                    .map(De)
+                    .map_err(|e| E::custom(format!("{:?}", e)))
             }
         }
 
@@ -132,7 +136,7 @@ impl Deserialize for De<Cookie> {
 
 impl Deserialize for De<Headers> {
     fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
-        where D: Deserializer
+        where D: Deserializer,
     {
         struct HeadersVisitor;
 
@@ -140,20 +144,21 @@ impl Deserialize for De<Headers> {
             type Value = De<Headers>;
 
             fn visit_unit<E>(&mut self) -> Result<Self::Value, E>
-                where E: Error
+                where E: Error,
             {
                 Ok(De(Headers::new()))
             }
 
-            fn visit_map<V>(&mut self, mut visitor: V)
+            fn visit_map<V>(&mut self,
+                            mut visitor: V)
                             -> Result<Self::Value, V::Error>
-                where V: MapVisitor
+                where V: MapVisitor,
             {
                 let mut headers = Headers::new();
-                while let Some((key, value)) = try!(visitor.visit::<String, _>()) {
+                while let Some((key, value)) = visitor.visit::<String, _>()? {
                     headers.set_raw(key, value);
                 }
-                try!(visitor.end());
+                visitor.end()?;
                 Ok(De(headers))
             }
         }
@@ -164,7 +169,7 @@ impl Deserialize for De<Headers> {
 
 impl Deserialize for De<Method> {
     fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
-        where D: Deserializer
+        where D: Deserializer,
     {
         struct MethodVisitor;
 
@@ -172,11 +177,11 @@ impl Deserialize for De<Method> {
             type Value = De<Method>;
 
             fn visit_str<E>(&mut self, v: &str) -> Result<Self::Value, E>
-                where E: Error
+                where E: Error,
             {
-                v.parse::<Method>().map(De).map_err(|err| {
-                    E::invalid_value(&err.to_string())
-                })
+                v.parse::<Method>()
+                    .map(De)
+                    .map_err(|err| E::invalid_value(&err.to_string()))
             }
         }
 
@@ -186,9 +191,9 @@ impl Deserialize for De<Method> {
 
 impl Deserialize for De<RawStatus> {
     fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
-        where D: Deserializer
+        where D: Deserializer,
     {
-        let (code, reason) = try!(Deserialize::deserialize(deserializer));
+        let (code, reason) = Deserialize::deserialize(deserializer)?;
         Ok(De(RawStatus(code, reason)))
     }
 }
@@ -198,7 +203,8 @@ impl Deserialize for De<RawStatus> {
 /// This is useful to serialize Hyper types used in structure fields or
 /// tuple members with `#[serde(serialize_with = "hyper_serde::serialize")]`.
 pub fn serialize<T, S>(value: &T, serializer: &mut S) -> Result<(), S::Error>
-    where S: Serializer, for<'a> Ser<'a, T>: Serialize
+    where S: Serializer,
+          for<'a> Ser<'a, T>: Serialize,
 {
     Ser::new(value).serialize(serializer)
 }
@@ -206,12 +212,14 @@ pub fn serialize<T, S>(value: &T, serializer: &mut S) -> Result<(), S::Error>
 /// A wrapper to serialize Hyper types.
 ///
 /// This is useful with functions such as `serde_json::to_string`.
-/// 
+///
 /// Values of this type can only be passed to the `serde::Serialize` trait.
 #[derive(Debug)]
 pub struct Ser<'a, T: 'a>(&'a T);
 
-impl<'a, T> Ser<'a, T> where Ser<'a, T>: serde::Serialize {
+impl<'a, T> Ser<'a, T>
+    where Ser<'a, T>: serde::Serialize,
+{
     /// Returns a new `Ser` wrapper.
     #[inline(always)]
     pub fn new(value: &'a T) -> Self {
@@ -221,7 +229,7 @@ impl<'a, T> Ser<'a, T> where Ser<'a, T>: serde::Serialize {
 
 impl<'a> Serialize for Ser<'a, ContentType> {
     fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
-        where S: Serializer
+        where S: Serializer,
     {
         (self.0).0.serialize(serializer)
     }
@@ -229,7 +237,7 @@ impl<'a> Serialize for Ser<'a, ContentType> {
 
 impl<'a> Serialize for Ser<'a, Cookie> {
     fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
-        where S: Serializer
+        where S: Serializer,
     {
         serializer.serialize_str(&self.0.to_string())
     }
@@ -237,14 +245,14 @@ impl<'a> Serialize for Ser<'a, Cookie> {
 
 impl<'a> Serialize for Ser<'a, Headers> {
     fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
-        where S: Serializer
+        where S: Serializer,
     {
-        let mut map_state = try!(serializer.serialize_map(Some(self.0.len())));
+        let mut map_state = serializer.serialize_map(Some(self.0.len()))?;
         for header in self.0.iter() {
             let name = header.name();
             let value = self.0.get_raw(name).unwrap();
-            try!(serializer.serialize_map_key(&mut map_state, name));
-            try!(serializer.serialize_map_value(&mut map_state, value));
+            serializer.serialize_map_key(&mut map_state, name)?;
+            serializer.serialize_map_value(&mut map_state, value)?;
         }
         serializer.serialize_map_end(map_state)
     }
@@ -252,7 +260,7 @@ impl<'a> Serialize for Ser<'a, Headers> {
 
 impl<'a> Serialize for Ser<'a, Method> {
     fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
-        where S: Serializer
+        where S: Serializer,
     {
         Serialize::serialize(self.0.as_ref(), serializer)
     }
@@ -260,7 +268,7 @@ impl<'a> Serialize for Ser<'a, Method> {
 
 impl<'a> Serialize for Ser<'a, RawStatus> {
     fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
-        where S: Serializer
+        where S: Serializer,
     {
         ((self.0).0, &(self.0).1).serialize(serializer)
     }
@@ -270,10 +278,12 @@ impl<'a> Serialize for Ser<'a, RawStatus> {
 /// a `Vec<T>` need to be passed to serde.
 #[derive(Clone, PartialEq)]
 pub struct Serde<T>(pub T)
-    where De<T>: Deserialize, for<'a> Ser<'a, T>: Serialize;
+    where De<T>: Deserialize,
+          for<'a> Ser<'a, T>: Serialize;
 
 impl<T> Serde<T>
-    where De<T>: Deserialize, for<'a> Ser<'a, T>: Serialize
+    where De<T>: Deserialize,
+          for<'a> Ser<'a, T>: Serialize,
 {
     /// Consumes this wrapper, returning the inner value.
     #[inline(always)]
@@ -283,7 +293,9 @@ impl<T> Serde<T>
 }
 
 impl<T> fmt::Debug for Serde<T>
-    where T: fmt::Debug, De<T>: Deserialize, for<'a> Ser<'a, T>: Serialize
+    where T: fmt::Debug,
+          De<T>: Deserialize,
+          for<'a> Ser<'a, T>: Serialize,
 {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         self.0.fmt(formatter)
@@ -291,7 +303,8 @@ impl<T> fmt::Debug for Serde<T>
 }
 
 impl<T> Deref for Serde<T>
-    where De<T>: Deserialize, for<'a> Ser<'a, T>: Serialize
+    where De<T>: Deserialize,
+          for<'a> Ser<'a, T>: Serialize,
 {
     type Target = T;
 
@@ -301,7 +314,8 @@ impl<T> Deref for Serde<T>
 }
 
 impl<T> DerefMut for Serde<T>
-    where De<T>: Deserialize, for<'a> Ser<'a, T>: Serialize
+    where De<T>: Deserialize,
+          for<'a> Ser<'a, T>: Serialize,
 {
     fn deref_mut(&mut self) -> &mut T {
         &mut self.0
@@ -309,7 +323,8 @@ impl<T> DerefMut for Serde<T>
 }
 
 impl<T: PartialEq> PartialEq<T> for Serde<T>
-    where De<T>: Deserialize, for<'a> Ser<'a, T>: Serialize
+    where De<T>: Deserialize,
+          for<'a> Ser<'a, T>: Serialize,
 {
     fn eq(&self, other: &T) -> bool {
         self.0 == *other
@@ -317,20 +332,22 @@ impl<T: PartialEq> PartialEq<T> for Serde<T>
 }
 
 impl<T> Deserialize for Serde<T>
-    where De<T>: Deserialize, for<'a> Ser<'a, T>: Serialize
+    where De<T>: Deserialize,
+          for<'a> Ser<'a, T>: Serialize,
 {
     fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
-        where D: Deserializer
+        where D: Deserializer,
     {
         De::deserialize(deserializer).map(De::into_inner).map(Serde)
     }
 }
 
 impl<T> Serialize for Serde<T>
-    where De<T>: Deserialize, for<'a> Ser<'a, T>: Serialize
+    where De<T>: Deserialize,
+          for<'a> Ser<'a, T>: Serialize,
 {
     fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
-        where S: Serializer
+        where S: Serializer,
     {
         Ser(&self.0).serialize(serializer)
     }
@@ -338,7 +355,7 @@ impl<T> Serialize for Serde<T>
 
 #[test]
 fn test_content_type() {
-    let content_type = ContentType(mime!(Application/Json));
+    let content_type = ContentType(mime!(Application / Json));
     let tokens = &[Token::Str("application/json")];
 
     assert_ser_tokens(&Ser::new(&content_type), tokens);
@@ -361,8 +378,8 @@ fn test_cookie() {
         custom: BTreeMap::new(),
     };
 
-    let tokens =
-        &[Token::Str("Hello=World!; Secure; Path=/; Domain=servo.org; Max-Age=42")];
+    let tokens = &[Token::Str("Hello=World!; Secure; Path=/; \
+                               Domain=servo.org; Max-Age=42")];
 
     assert_ser_tokens(&Ser::new(&cookie), tokens);
     assert_de_tokens(&De(cookie), tokens);
@@ -372,10 +389,7 @@ fn test_cookie() {
 fn test_headers_empty() {
     let headers = Headers::new();
 
-    let tokens = &[
-        Token::MapStart(Some(0)),
-        Token::MapEnd,
-    ];
+    let tokens = &[Token::MapStart(Some(0)), Token::MapEnd];
 
     assert_ser_tokens(&Ser::new(&headers), tokens);
     assert_de_tokens(&De(headers), tokens);
@@ -386,30 +400,39 @@ fn test_headers_not_empty() {
     use hyper::header::Host;
 
     let mut headers = Headers::new();
-    headers.set(Host { hostname: "baguette".to_owned(), port: None });
+    headers.set(Host {
+        hostname: "baguette".to_owned(),
+        port: None,
+    });
 
     // In Hyper 0.9, Headers is internally a HashMap and thus testing this
     // with multiple headers is non-deterministic.
 
-    let tokens = &[
-        Token::MapStart(Some(1)),
-            Token::MapSep,
-                Token::Str("Host"),
-                Token::SeqStart(Some(1)),
-                    Token::SeqSep,
-                        Token::SeqStart(Some(8)),
-                            Token::SeqSep, Token::U8(98),  // 'b'
-                            Token::SeqSep, Token::U8(97),  // 'a'
-                            Token::SeqSep, Token::U8(103), // 'g'
-                            Token::SeqSep, Token::U8(117), // 'u'
-                            Token::SeqSep, Token::U8(101), // 'e'
-                            Token::SeqSep, Token::U8(116), // 't'
-                            Token::SeqSep, Token::U8(116), // 't'
-                            Token::SeqSep, Token::U8(101), // 'e'
-                    Token::SeqEnd,
-                Token::SeqEnd,
-        Token::MapEnd,
-    ];
+    let tokens = &[Token::MapStart(Some(1)),
+                   Token::MapSep,
+                   Token::Str("Host"),
+                   Token::SeqStart(Some(1)),
+                   Token::SeqSep,
+                   Token::SeqStart(Some(8)),
+                   Token::SeqSep,
+                   Token::U8(98), // 'b'
+                   Token::SeqSep,
+                   Token::U8(97), // 'a'
+                   Token::SeqSep,
+                   Token::U8(103), // 'g'
+                   Token::SeqSep,
+                   Token::U8(117), // 'u'
+                   Token::SeqSep,
+                   Token::U8(101), // 'e'
+                   Token::SeqSep,
+                   Token::U8(116), // 't'
+                   Token::SeqSep,
+                   Token::U8(116), // 't'
+                   Token::SeqSep,
+                   Token::U8(101), // 'e'
+                   Token::SeqEnd,
+                   Token::SeqEnd,
+                   Token::MapEnd];
 
     assert_ser_tokens(&Ser::new(&headers), tokens);
     assert_de_tokens(&De(headers), tokens);
@@ -429,12 +452,12 @@ fn test_raw_status() {
     use std::borrow::Cow;
 
     let raw_status = RawStatus(200, Cow::Borrowed("OK"));
-    let tokens = &[
-        Token::TupleStart(2),
-            Token::TupleSep, Token::U16(200),
-            Token::TupleSep, Token::Str("OK"),
-        Token::TupleEnd,
-    ];
+    let tokens = &[Token::TupleStart(2),
+                   Token::TupleSep,
+                   Token::U16(200),
+                   Token::TupleSep,
+                   Token::Str("OK"),
+                   Token::TupleEnd];
 
     assert_ser_tokens(&Ser::new(&raw_status), tokens);
     assert_de_tokens(&De(raw_status), tokens);
